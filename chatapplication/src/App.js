d7a5af0acb9d4568014fe5a43296591afc0415de
TaliaOh <taloh1503@gmail.com>
2023-04-23 16:40:39 +0300

    build the chat app

--- chatapplication/src/App.js
+++ chatapplication/src/App.js
@@ -1,7 +1,28 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const projectID = "ceb39722-0d19-4894-bdc0-259406873f4f";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
 
 export default App;
